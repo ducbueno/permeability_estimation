be58e738b5c0b34d2ab4febe8e311fc43c648119
José Eduardo Bueno <ducbueno@gmail.com>
2025-02-06 15:39:07 -0300

    Simplify subarray sampler

--- subarray_sampler.py
+++ subarray_sampler.py
@@ -1,35 +1,13 @@
 import math
-import re
-from pathlib import Path
 
 import numpy as np
 from scipy.stats import qmc
 
 
 class SubarraySampler:
-    def __init__(self, file_path, dtype=np.uint8, invert=False):
-        self.dtype = dtype
-        self.file_path = Path(file_path)
+    def __init__(self, array, invert=False):
         self.invert = invert
-        if not self.file_path.exists():
-            raise FileNotFoundError(f"File {file_path} not found")
-
-        dims_match = re.search(r"(\d+)x(\d+)x(\d+)", self.file_path.name)
-        if not dims_match:
-            raise ValueError("Filename must contain dimensions in format: n1xn2xn3")
-
-        self.dims = tuple(int(dim) for dim in dims_match.groups())
-        self._array = None
-
-    @property
-    def array(self):
-        if self._array is None:
-            self._array = np.fromfile(self.file_path, dtype=self.dtype).reshape(
-                self.dims
-            )
-            if self.invert:
-                self._array = (~self._array.astype(bool)).astype(np.uint8)
-        return self._array
+        self.array = array
 
     def sample_random(self, num_samples: int, factor: int = 10):
         """Sample subarrays using Latin Hypercube Sampling.
